c4b257c580fdf6eeba4c7c56c67e9960798dbcc6
Hirpoji <roadtothejun@gmail.com>
2023-06-30 18:16:37 +0300

    test

--- frontend/src/pages/FullPost.tsx
+++ frontend/src/pages/FullPost.tsx
@@ -11,6 +11,7 @@ const getRandomColor = () => {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
+  console.log(1);
   return `rgb(${r}, ${g}, ${b})`;
 };
 
